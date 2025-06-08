80c7335dcf1dd2c0e21b3a7ffeb99d6405621120
Nasir Masud <nasir.masud@ymail.com>
2025-06-08 19:50:40 +0600

    Update Home component layout and image handling for improved presentation

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,22 +1,28 @@
+import Logo from "@/components/logo";
 import Image from "next/image";
+import previewImage from "../../../public/PCC.jpg";
 
 export default function Home() {
   return (
-    <main className='bg-[#00c98c] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10'>
+    <main className='bg-[#0f5b72] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10'>
       <Image
-        src='/PCC.jpg'
+        src={previewImage}
         alt='Preview of Next petCare'
         width={520}
         height={470}
         priority
       />
       <div>
+        <Logo />
         <h1 className='text-3xl font-bold text-center text-slate-900'>
           Welcome to Pet Care
         </h1>
         <p className='text-center text-slate-700'>
           Your one-stop solution for all your pet care needs.
         </p>
+        <div>
+          <button>SignUp</button>
+        </div>
       </div>
     </main>
   );

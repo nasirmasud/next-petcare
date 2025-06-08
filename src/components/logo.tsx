4785138d68b7cbac60047958597d6615aab7268e
Nasir Masud <nasir.masud@ymail.com>
2025-06-08 21:09:40 +0600

    Update Logo component image dimensions to 42x42

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ import logo from "../../public/icon.svg";
 export default function Logo() {
   return (
     <Link href='/'>
-      <Image src={logo} alt='NextPetCare Logo' height={48} width={48} />
+      <Image src={logo} alt='NextPetCare Logo' height={42} width={42} />
     </Link>
   );
 }
